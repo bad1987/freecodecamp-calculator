f94cefde2308affc8e2778038bb933a123f47d76
badinfo1987 <badinfo1987@gmail.com>
2020-08-19 15:23:45 +0100

    Updates

---
+++ precache-manifest.38a0020ea5b2c98f2e776a20abe18dba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8a1126c2f0312969a09b7515687de2",
+    "url": "/freecodecamp-calculator/index.html"
+  },
+  {
+    "revision": "f6a2724e04944442f641",
+    "url": "/freecodecamp-calculator/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8cfe954b4f051944f965",
+    "url": "/freecodecamp-calculator/static/css/main.92487366.chunk.css"
+  },
+  {
+    "revision": "f6a2724e04944442f641",
+    "url": "/freecodecamp-calculator/static/js/2.cb84c3b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freecodecamp-calculator/static/js/2.cb84c3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cfe954b4f051944f965",
+    "url": "/freecodecamp-calculator/static/js/main.9d4d917f.chunk.js"
+  },
+  {
+    "revision": "ea2b1d5eda3fb6d846e3",
+    "url": "/freecodecamp-calculator/static/js/runtime-main.82a57f7e.js"
+  }
+]);
